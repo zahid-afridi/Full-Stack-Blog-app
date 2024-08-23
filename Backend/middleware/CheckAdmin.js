740d46bd2d7aefde19fdb45c6d09cffcd2d21029
Muhammad Zahid <zahidtime313@gmail.com>
2024-08-23 07:49:21 -0700

    add is login

--- Backend/middleware/CheckAdmin.js
+++ Backend/middleware/CheckAdmin.js
@@ -34,7 +34,34 @@ const isAdmin = async (req, res, next) => {
         return res.status(401).json({ message: 'Unauthorized: Invalid token' });
     }
 };
+const isLogin = async (req, res, next) => {
+    try {
+        const token = req.cookies.token;
+
+        if (!token) {
+            
+            return res.status(401).json({ message: 'Unauthorized: Please Login ' });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        
+        console.log('Decoded token:', decoded); // Log decoded token payload
+
+        const user = await UserModal.findById(decoded.userId);
+        console.log('User:', user);
+
+        if (!user) {
+            return res.status(403).json({ message: 'Unauthorized: User not found' });
+        }
 
 
+        req.user = user;
+        next();
+    } catch (error) {
+        console.error('Error verifying token:', error);
+        return res.status(401).json({ message: 'Unauthorized: Invalid token' });
+    }
+};
 
-export {isAdmin}
+
+export {isAdmin,isLogin}
--- Backend/routes/Comments.js
+++ Backend/routes/Comments.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import { AddComment } from '../controllers/Comments.js'
+import { isLogin } from '../middleware/CheckAdmin.js'
 
 const CommentRoutes=express.Router()
 
-CommentRoutes.post('/addcomment',AddComment)
+CommentRoutes.post('/addcomment',isLogin,AddComment)
 
 export default CommentRoutes
